12ac505f8e5b86bb48a3fe437a2fbbbea2aa8512
lenovo <lenovo@DESKTOP-UP83FQL>
2019-10-01 22:21:57 +0530

    01 commit

---
+++ MvcLoginForm/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcLoginForm.Models;
+
+namespace MvcLoginForm.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Model objmodel)
+        {
+            if (objmodel.userName=="Maddineni" && objmodel.password=="Anjineyulu")
+            return RedirectToAction("WelcomePage");
+            else
+            {
+                objmodel.Message = "Invalid username/Password";
+                return View(objmodel);
+            }  
+          }
+        public ActionResult WelcomePage()
+        {
+            return View();
+        }
+    }
+}
---
+++ MvcLoginForm/Models/Model.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcLoginForm.Models
+{
+    public class Model
+    {
+        public string userName { get; set; }
+        public string password { get; set; }
+        public string Message { get; set; }
+    }
+}
